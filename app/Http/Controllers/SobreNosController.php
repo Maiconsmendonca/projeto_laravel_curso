a91eba2b8d8821011e5a1329044a2b8c885ebcb7
Maicon Mendonca <maicon.mendonca@hotmail.com>
2022-03-18 18:09:08 -0300

    inclusos middlewares

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class SobreNosController extends Controller
 {
+    public function __construct() {
+        $this->middleware('log.acesso');
+    }
+
     public function sobrenos(){
         return view('site.sobre-nos', ['titulo'=>'Sobre Nós']);
     }
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])->name('site.index');
+Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])->name('site.index')->middleware('log.acesso');
+
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
+
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
 Route::post('/contato', [\App\Http\Controllers\ContatoController::class,'salvar'])->name('site.contato');
+
 Route::get('/login', function (){return 'Login'; })->name('app.login');
 
-Route::prefix('/app')->group(function (){
+Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function (){
     Route::get('/clientes', function (){return 'Clientes'; })->name('app.clientes');
     Route::get('/fornecedores', function (){return 'Fornecedores'; })->name('app.fornecedores');
     Route::get('/produtos', function (){return 'Produtos'; })->name('app.produtos');
